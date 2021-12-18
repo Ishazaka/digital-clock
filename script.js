e52c827181538fda8a53d62200d3adfb279cf127
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2021-12-18 07:54:08 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+
+ const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+function digitalclock(){
+
+let h = new Date().getHours();
+ let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+
+ if(h >=12){ 
+     h = ((h - 12) == 0) ? 12: (h-12); 
+   }else{ 
+      h = h; 
+   }
